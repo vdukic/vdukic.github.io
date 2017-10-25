db5143afa373ef29ca37845a0a5fcaed3138a8ee
Vladimir <vdukic1586@gmail.com>
2017-10-25 16:39:53 +0200

    Update resume

---
+++ app.js
@@ -0,0 +1,16 @@
+(function() {
+    document.getElementById("firstButton").click();
+})();
+function openSkill(evt, skillName){
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(skillName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
