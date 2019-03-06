39a7f35229b5851b30f7704dc7a0c3b069db7dc0
vdukic <vladarix@yahoo.com>
2019-03-06 11:43:16 +0100

    Projects

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 (function() {
     document.getElementById("firstButton").click();
+    document.getElementById("firstProject").click();
 })();
 function openSkill(evt, skillName){
     var i, tabcontent, tablinks;
@@ -13,4 +14,21 @@ function openSkill(evt, skillName){
     }
     document.getElementById(skillName).style.display = "block";
     evt.currentTarget.className += " active";
+}
+
+function openProject(evt, skillName){
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontentProject");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinksProject");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(skillName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function openInNewTab(e){
 }
